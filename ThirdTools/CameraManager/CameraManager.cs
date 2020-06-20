6784b5c7e0c208428ccc4706b2ba33c9ecc54548
兴荣 <956515072@qq.com>
2020-06-20 16:26:28 +0800

    摄像机

---
+++ ThirdTools/CameraManager/CameraManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Cameras;
+
+public class CameraManager : SingleBhv<CameraManager> {
+    public FreeLookCam freeLookCam; 
+    public Camera myCamera;
+
+    // Use this for initialization
+//    void Awake ()
+//	{
+//	    freeLookCam = GetComponentInChildren<FreeLookCam>();
+//        myCamera = GetComponentInChildren<Camera>();
+//	}
+
+}
---
+++ ThirdTools/CameraManager/CameraObstacleDetecter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Cameras;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class CameraObstacleDetecter : MonoBehaviour
+{
+    private Transform _target;
+    private BoxCollider triggerCollider;
+    private Transform eyeCameraTrans;
+//    public LayerMask layerMask;
+    public string obstacleTag = "Obstacle";
+    public Material transparentMaterial;
+    Dictionary<MeshRenderer, Material> materialCache = new Dictionary<MeshRenderer, Material>();
+
+    Transform target
+    {
+        get
+        {
+            if (_target)
+            {
+                return _target;
+            }
+            _target = GetComponentInParent<FreeLookCam>()?.Target;
+            return _target;
+        }
+        set { _target = value; }
+    }
+
+    private void Awake()
+    {
+        triggerCollider = GetComponent<BoxCollider>();
+        GetComponent<Rigidbody>().isKinematic = true;
+        triggerCollider.isTrigger = true;
+        eyeCameraTrans = GetComponentInParent<Camera>().transform;
+    }
+
+    private void LateUpdate()
+    {
+        if (eyeCameraTrans && triggerCollider && target)
+        {
+            triggerCollider.transform.position = (target.position + eyeCameraTrans.position)/2;
+            triggerCollider.size = new Vector3(triggerCollider.size.x, triggerCollider.size.y, Vector3.Distance(target.position, eyeCameraTrans.position));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger) return;
+        if (other.gameObject.tag.Equals(obstacleTag, StringComparison.OrdinalIgnoreCase))
+        {
+            ChangeMaterials(other);
+        }
+    }
+
+    private void ChangeMaterials(Collider other)
+    {
+        var render = other.GetComponent<MeshRenderer>();
+        if (render != null)
+        {
+            materialCache[render] = render.sharedMaterial;
+            render.material = transparentMaterial;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.isTrigger) return;
+        if (other.gameObject.tag.Equals(obstacleTag, StringComparison.OrdinalIgnoreCase))
+        {
+            ChangeBackMaterials(other);
+        }
+    }
+
+    private void ChangeBackMaterials(Collider other)
+    {
+        var render = other.GetComponent<MeshRenderer>();
+        if (render != null)
+        {
+            if (materialCache.ContainsKey(render)) render.sharedMaterial = materialCache[render];
+        }
+    }
+}
