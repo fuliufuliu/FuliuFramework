908d18351b02bf0a61de7cfafec24d312e7a6a7f
兴荣 <fuliufuliu@aliyun.com>
2020-06-30 14:17:14 +0800

    继续使用新版本的dotween

---
+++ ThirdTools/DOTween/Test/TestDotween.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class TestDotween : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(ChangeSpeed());
+    }
+
+    private IEnumerator ChangeSpeed()
+    {
+        yield return new WaitForSeconds(Random.Range(1.3f, 1.5f)); 
+        var tween = transform.DOLocalMove(new Vector3(80, 5, 253f), 4).SetLoops(1, LoopType.Yoyo).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(Random.Range(1.3f, 1.5f));
+        tween.ChangeValues(transform.localPosition, new Vector3(80, 5, 253f), 0.5f);
+    }
+}
