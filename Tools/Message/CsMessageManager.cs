762e955568d7862df5de5efd06826438506d0c21
兴荣 <fuliufuliu@aliyun.com>
2020-07-07 15:22:32 +0800

    简化消息系统

--- Tools/Message/CsMessageManager.cs
+++ Tools/Message/CsMessageManager.cs
@@ -11,24 +11,16 @@ namespace CsMessage
     public delegate void CallFun(params object[] senders);
     
     public class CsMessageManager {
+
         /// <summary>
-        /// key:定义的原始消息，value：组合的消息(key:id 或GameObject对应的GetInstanceID，value：组合Id)
-        /// </summary>
-        static Dictionary<string, Dictionary<int,string>> _msgKeyDic = new Dictionary<string, Dictionary<int, string>>();
-        /// <summary>
-        /// key:定义的原始消息，value：组合的消息(key: 某个接收消息的 组件，value：接收消息的方法)
+        /// key1:定义的原始消息，key2: 某个接收消息的 组件，value：事件
         /// </summary>
         static Dictionary<string, Dictionary<Component, CallFun>> componentCallFuncDic = new Dictionary<string, Dictionary<Component, CallFun>>();
+
         /// <summary>
-        /// key：组合的消息 ，value：调用的方法
+        /// key1:定义的原始消息，key2: 某个接收消息的 对象的ID，value：事件
         /// </summary>
-        static Dictionary<string, CallFun> _msgDic = new Dictionary<string, CallFun>();
-        /// <summary>
-        /// 事件缓存
-        /// </summary>
-        private static List<CallFun> eventListTemp = new List<CallFun>(20);
-    
-        private static List<CallFun> eventList = new List<CallFun>(8);
+        static Dictionary<string, Dictionary<int, CallFun>> msgIdEventDic = new Dictionary<string, Dictionary<int, CallFun>>();
         
         public static Action<string> onLogError { get; set; }
         public static Action<string> onLogWarning { get; set; }
@@ -39,25 +31,6 @@ namespace CsMessage
             onLogWarning = Debug.LogWarning;
         }
     
-        /// <summary>
-        /// 监听消息，CallFunNull不回调参数  CallFun带一个参数 CallFun2 带2个参数
-        /// </summary>
-        /// <param name="msg"></param>
-        /// <param name="fun"></param>
-        public static void addMessage(string msg, CallFun fun, GameObject target=null,int id=100000)
-        {
-    		int targetId=target== null ?  id : target.GetInstanceID();
-    		var msgEx = msg +"#"+ targetId;
-            if (!_msgKeyDic.ContainsKey(msg)) {
-                _msgKeyDic[msg] = new Dictionary<int, string>();
-            }
-            if (_msgKeyDic.ContainsKey(msg) && _msgKeyDic[msg].ContainsKey(targetId)) {
-                onLogWarning?.Invoke(msgEx + "可能没有删!");
-            }
-            _msgKeyDic[msg][targetId] = msgEx;
-            _msgDic[msgEx] = fun;
-        }
-
         public static void addHierarchyMessage(string msg, CallFun fun, Component trans)
         {
             if (! componentCallFuncDic.ContainsKey(msg)) 
@@ -73,54 +46,38 @@ namespace CsMessage
                     componentCallFuncDic[msg].Remove(trans);
         }
         
-        public static void addMessage(string msg, CallFun fun, MonoBehaviour target=null,int id=100000)
+        public static void addMessage(string msg, CallFun fun, MonoBehaviour target=null, int id=100000)
         {
-            int targetId=target== null ?  id : target.GetInstanceID();
-            var msgEx = msg +"#"+ targetId;
-            if (!_msgKeyDic.ContainsKey(msg)) {
-                _msgKeyDic[msg] = new Dictionary<int, string>();
+            int targetId = target == null ?  id : target.GetInstanceID();
+            if (! msgIdEventDic.ContainsKey(msg))
+            {
+                msgIdEventDic[msg] = new Dictionary<int, CallFun>();
             }
-            if (_msgKeyDic.ContainsKey(msg) && _msgKeyDic[msg].ContainsKey(targetId)) {
-                onLogWarning?.Invoke(msgEx + "可能没有删!");
+
+            if (msgIdEventDic[msg].ContainsKey(targetId))
+            {
+                onLogWarning?.Invoke($"{msg} # {targetId} 可能没有删!");
             }
-            _msgKeyDic[msg][targetId] = msgEx;
-            _msgDic[msgEx] = fun;
+            msgIdEventDic[msg][targetId] = fun;
         }
     
         /// <summary>
         ///  发送消息
         /// </summary>
-        /// <param name="msg"></param>
-        /// <param name="sender"></param>
         public static void sendMessage(string msg, params object[] senders)
         {
-            if (_msgKeyDic.ContainsKey(msg))
+            if (msgIdEventDic.ContainsKey(msg))
             {
-                var keys = _msgKeyDic[msg];
-                eventList.Clear();
-                foreach (var keyPair in keys)
+                var idEvents = msgIdEventDic[msg];
+                foreach (var idEventPair in idEvents)
                 {
-                    if (_msgDic.ContainsKey(keyPair.Value))
+                    try
                     {
-                        eventList.Add(_msgDic[keyPair.Value]);
+                        idEventPair.Value(senders);
                     }
-                }
-    
-                if (eventList.Count > 0)
-                {
-                    for (int i = 0; i < eventList.Count; i++)
+                    catch (Exception ex)
                     {
-                        try
-                        {
-                            if (eventList[i] != null)
-                            {
-                                eventList[i](senders);
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            onLogError?.Invoke("CsMessageManager::::" + ex);
-                        }
+                        onLogError?.Invoke("CsMessageManager::::" + ex);
                     }
                 }
             }
@@ -169,64 +126,38 @@ namespace CsMessage
         /// </summary>
         /// <param name="msg"></param>
         /// <param name="isBubb"></param>
-    	public static void remove(string msg, GameObject target=null,int id=100000) {
-            int targetId = target == null ? id : target.GetInstanceID();
-    		var msgEx = msg +"#"+ targetId;
-            _msgDic.Remove(msgEx);
-            if (_msgKeyDic.ContainsKey(msg)) {
-                _msgKeyDic[msg].Remove(targetId);
-            }
-        }
-        
-        /// <summary>
-        /// 移除消息
-        /// </summary>
-        /// <param name="msg"></param>
-        /// <param name="isBubb"></param>
         public static void remove(string msg, MonoBehaviour target=null,int id=100000) {
             int targetId = target == null ? id : target.GetInstanceID();
-            var msgEx = msg +"#"+ targetId;
-            _msgDic.Remove(msgEx);
-            if (_msgKeyDic.ContainsKey(msg)) {
-                _msgKeyDic[msg].Remove(targetId);
+            if (msgIdEventDic.ContainsKey(msg))
+            {
+                if (msgIdEventDic[msg].ContainsKey(targetId))
+                {
+                    msgIdEventDic[msg].Remove(targetId);
+                }
             }
         }
         
     	public static void removeAll()
     	{
-            _msgDic.Clear();
-            _msgKeyDic.Clear();
+            msgIdEventDic.Clear();
         }
-        /// <summary>
-        /// 根据组合的msgEx分离得到原始msg
-        /// </summary>
-        /// <param name="msgEx">组合的msgEx</param>
-    	private static string getMessage(string msgEx)
-    	{
-    		char[] split={'#'};
-    		return msgEx.Split(split)[0];
-    	}
-        /// <summary>
-        /// 根据组合的msgEx分离得到调用的方法
-        /// </summary>
-    	private static CallFun getFun(string msgEx) {
-    	    if (_msgDic.ContainsKey(msgEx)) {
-    	        return _msgDic[msgEx];
-    	    }
-    		return null;
-    	}
-    
+        
     	/// <summary>
     	/// 判断是否存在该监听
     	/// </summary>
     	public static bool hasMessage(string msgEx) {
-    	    return _msgDic.ContainsKey(msgEx);
-    //        return _mapList.hasMap(msg);
+    	    return msgIdEventDic.ContainsKey(msgEx) && msgIdEventDic[msgEx].Count > 0;
     	}
+        
+        public static bool hasMessage(string msgEx, MonoBehaviour target=null,int id=100000) {
+            int targetId = target == null ? id : target.GetInstanceID();
+            return msgIdEventDic.ContainsKey(msgEx) && msgIdEventDic[msgEx].ContainsKey(targetId);
+        }
     
-        protected void OnDestroy() {
-            _msgDic = null;
-            _msgKeyDic = null;
+        protected void OnDestroy()
+        {
+            msgIdEventDic = null;
+            componentCallFuncDic = null;
         }
     }
 }
