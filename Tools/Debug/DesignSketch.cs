3c86f2e54ae939aba81e50d84df6361336090a42
兴荣 <fuliufuliu@aliyun.com>
2020-08-05 14:12:28 +0800

    这个工具并不能正常工作，待以后看能不能解决

---
+++ Tools/Debug/DesignSketch.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 效果图控制
+/// </summary>
+public class DesignSketch : MonoBehaviour
+{
+    private Thread myThread;
+    private System.Random myRandom;
+    public bool isRun = true;
+    private Timer timer;
+
+    private void OnValidate()
+    {
+        Debug.Log("#####################");
+        var myRawImage = GetComponent<RawImage>();
+
+        if (myRawImage)
+        {
+            if (myRandom == null)
+            {
+                myRandom = new System.Random();
+            }
+
+            if (timer != null)
+            {
+                timer.Dispose();
+                timer = null;
+            }
+            
+            timer = new Timer((a) =>
+            {
+                if (isRun)
+                {
+                    myRawImage.color = new Color(1,1,1, myRandom.Next(0, 100)/100f);
+                    myRawImage.enabled = !myRawImage.enabled;
+                }
+
+            }, null, 500, 500);
+        }
+    }
+}
